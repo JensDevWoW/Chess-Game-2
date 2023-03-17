f56cfb1f105d02ca1c5653b96120a2d624381c29
Jens <jordan83222@gmail.com>
2023-03-17 08:37:03 -0500

    Massive update to Negamax function
    Implement Null Move Pruning, AEL Pruning, Enhanced Forward Pruning, and PVS Search (UNTESTED)

--- engine.py
+++ engine.py
@@ -54,42 +54,97 @@ class Engine:
         self.negamax(tboard, validMoves, DEPTH, -1)
         return nextMove
 
-    def negamax(self, tboard, validMoves, depth, turnMultiplier):
-        global nextMove, counter
-        counter += 1
-        if depth == 0:
-            return turnMultiplier * tboard.evaluate()
-        maxScore = -1000
-        for move in validMoves:
-            piece = move.piece
-            tboard.move(piece, move)
-            nextMoves = tboard.getValidMoves()
-            score = -self.negamax(tboard, nextMoves, depth - 1, -turnMultiplier)
-            if score > maxScore:
-                maxScore = score
-                if depth == DEPTH:
-                    nextMove = move
-            tboard.undo_minimax_move(piece, move)
-        return maxScore
+    def negamax_ab(self, tboard, validMoves, depth, alpha, beta, turnMultiplier,     null_move_allowed=True, is_root=False):
+      global nextMove, counter
+      counter += 1
+      
+      if depth == 0:
+          return turnMultiplier * tboard.evaluate()
+  
+      # Null move pruning
+      R = 2  # Reduction factor
+      
+      if null_move_allowed and depth >= R + 1 and not tboard.in_check():
+          tboard.make_null_move()
+          score = -self.negamax_ab(tboard, validMoves, depth - R - 1, -beta, -beta + 1, -turnMultiplier, False, False)
+          tboard.undo_null_move()
+          
+          if score >= beta:
+              return score
+  
+      maxScore = float('-inf')
+      first_move = True
+      
+      for move in validMoves:
+          # AEL-Pruning
+          if self.ael_pruning(move, depth, alpha):
+              continue
+  
+          # Enhanced Forward Pruning
+          if self.efp_pruning(move, depth, alpha, beta):
+              continue
+  
+          piece = move.piece
+          tboard.move(piece, move)
+          nextMoves = tboard.getValidMoves()
+  
+          if first_move:
+              first_move = False
+              score = -self.negamax_ab(tboard, nextMoves, depth - 1, -beta, -alpha, -turnMultiplier, True, False)
+          else:
+              # PVS search
+              score = -self.negamax_ab(tboard, nextMoves, depth - 1, -(alpha + 1), -alpha, -turnMultiplier, True, False)
+              
+              if alpha < score < beta:
+                  score = -self.negamax_ab(tboard, nextMoves, depth - 1, -beta, -alpha, -turnMultiplier, True, False)
+  
+          tboard.undo_move(piece, move)
+  
+          if score > maxScore:
+              maxScore = score
+              
+              if is_root:
+                  nextMove = move
+  
+          if maxScore > alpha:
+              alpha = maxScore
+              
+          if alpha >= beta:
+              break
+  
+      return maxScore
+  
+    def efp_pruning(self, move, depth, alpha, beta):
+      if depth > 2 and self.is_less_promising(move):
+          margin = (beta - alpha) / 2
+          
+          if self.ael_pruning(move, depth, alpha + margin):
+              return True
+          
+      return False
+    
+    def ael_pruning(self, move, depth, alpha):
+      # AEL pruning
+      if move.promotion or move.captured_piece is not None:
+          return False
+  
+      if depth >= 3:
+          if move.piece.piece_type == chess.PAWN:
+              return False
+          elif move.piece.piece_type == chess.KNIGHT and alpha >= self.knight_values[depth]:
+              return True
+          elif move.piece.piece_type == chess.BISHOP and alpha >= self.bishop_values[depth]:
+              return True
+          elif move.piece.piece_type == chess.ROOK and alpha >= self.rook_values[depth]:
+              return True
+          elif move.piece.piece_type == chess.QUEEN and alpha >= self.queen_values[depth]:
+              return True
+  
+      return False
 
-    def negamax_ab(self, tboard, validMoves, depth, alpha, beta, turnMultiplier):
-        global nextMove, counter
-        counter += 1
-        if depth == 0:
-            return turnMultiplier * tboard.evaluate()
-        maxScore = -1000
-        for move in validMoves:
-            piece = move.piece
-            tboard.move(piece, move)
-            nextMoves = tboard.getValidMoves()
-            score = -self.negamax_ab(tboard, nextMoves, depth - 1, -beta, -alpha, -turnMultiplier)
-            if score > maxScore:
-                maxScore = score
-                if depth == DEPTH:
-                    nextMove = move
-            tboard.undo_minimax_move(piece, move)
-            if maxScore > alpha:
-                alpha = maxScore
-            if alpha >= beta:
-                break
-        return maxScore
+    
+    def is_less_promising(self, move):
+      # Implement your heuristic here to determine if a move is less promising.
+      # For example, you can consider moves that capture lower-valued pieces as less promising.
+      # This is just a simple example and may not be the most effective heuristic.
+      return move.captured_piece is not None and move.captured_piece.value < move.piece.value
